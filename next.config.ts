e88059ba773ee9feb107ea08f2190ada738354b0
chyun5197 <chyun5197@gmail.com>
2026-05-19 22:12:20 +0900

    hotfix: 컨텐츠 리스트 넥스트 이미지 최적화 off, PreviewOverlay 그라데이션 오버레이 제거

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ const nextConfig: NextConfig = {
         ignoreBuildErrors: true,
     },
     images: {
+        // TMDB CDN이 이미 사이즈별로 최적화된 이미지를 제공하므로
+        // Next.js Image Optimization을 우회 (서버 메모리/지연 비용 절감)
+        unoptimized: true,
         remotePatterns: [
             {
                 protocol: 'https',
